13ad33920cf8e11ce4d3f597562665ad34d4196f
Kasper Christensen <kgc@bgs.dk>
2020-02-27 14:30:27 +0100

    Created 1 serialization scenario

---
+++ Assets/CodeOnly/Serialization_Scenario1/Health.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+namespace com.brainplus.jobtest
+{
+    public class Health : MonoBehaviour
+    {
+        public int maxHealth = 100; // TODO: Make readonly for other classes, while still serialized (tweakable in the inspector)
+        public int currentHealth = 0; // TODO: Make readonly for other classes
+
+        /// <summary>
+        /// You are not allowed to change this function.
+        /// </summary>
+        void Awake()
+        {
+            // Start with max health
+            currentHealth = maxHealth;
+        }
+
+        /// <summary>
+        /// You are not allowed to change this function.
+        /// </summary>
+        public void TakeDamage(int damage)
+        {
+            // Subtract damage from health, ensuring we cant go below 0
+            currentHealth = Mathf.Max(0, currentHealth - damage);
+        }
+
+        /// <summary>
+        /// You are not allowed to change this function.
+        /// </summary>
+        public bool IsDead
+        {
+            get
+            {
+                return currentHealth <= 0;
+            }
+        }
+    }
+}
---
+++ Assets/CodeOnly/Serialization_Scenario1/Serialization_Scenario1.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+namespace com.brainplus.jobtest
+{
+    /// <summary>
+    /// Change the <see cref="Health"/> class, so that its important internal variables cannot be
+    /// changed directly from the outside, but instead must use the <see cref="Health.TakeDamage(int)"/> function.
+    /// </summary>
+    public class Serialization_Scenario1 : MonoBehaviour
+    {
+        public Health health;
+
+        /// <summary>
+        /// You are not allowed to change this function.
+        /// </summary>
+        void Start()
+        {
+            KillPlayer();
+        }
+
+        void KillPlayer()
+        {
+            health.currentHealth = 0; // TODO: Prevent changing currentHealth like this
+            health.TakeDamage(health.currentHealth); // TODO: But allow reading currentHealth like this
+        }
+    }
+}
