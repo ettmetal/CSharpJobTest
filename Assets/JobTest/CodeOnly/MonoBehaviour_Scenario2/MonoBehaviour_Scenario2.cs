89ada7e47d98f6400d17190cbd331c03888a6a61
Kasper Christensen <kgc@bgs.dk>
2020-02-27 16:32:51 +0100

    Created monobehaviour scenario for optimizing overhead of many instances

---
+++ Assets/JobTest/CodeOnly/MonoBehaviour_Scenario2/MonoBehaviour_Scenario2.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace com.brainplus.jobtest.monobehaviour.scenario2
+{
+    /// <summary>
+    /// Optimize the way we are calling <see cref="RandomCounter.TickCounter"/>. Note that we spawn a lot of instances on purpose
+    /// to provoke the performance problem.
+    /// Hint: There is a more optimal way to call the <see cref="RandomCounter.TickCounter"/> function this many times each frame.
+    /// </summary>
+    public class MonoBehaviour_Scenario2 : JobTestMonoBehaviour
+    {
+        public override string Instructions => "Optimize the way we are calling RandomCounter.TickCounter(). Note that we spawn a lot of instances on purpose to provoke the performance problem. Hint: There is a more optimal way to call the TickCounter function this many times each frame.";
+
+        /// <summary>
+        /// You are not allowed to change this function.
+        /// </summary>
+        void Start()
+        {
+            SpawnRandomCounters();
+        }
+
+        void SpawnRandomCounters()
+        {
+            for (int i = 0; i < 100000; i++) // You are not allowed to spawn fewer instances of RandomCounter
+            {
+                GameObject go = new GameObject(nameof(RandomCounter));
+                go.AddComponent<RandomCounter>();
+            }
+        }
+    }
+}
---
+++ Assets/JobTest/CodeOnly/MonoBehaviour_Scenario2/RandomCounter.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace com.brainplus
+{
+    public class RandomCounter : MonoBehaviour
+    {
+        float waitDuration = 1f;
+        float lastTime = 0f;
+
+        public int Count { get; private set; } = 0;
+
+        /// <summary>
+        /// You are not allowed to change this function.
+        /// </summary>
+        void Awake()
+        {
+            waitDuration = Random.Range(1f, 10f);
+        }
+
+        void Update()
+        {
+            TickCounter();
+        }
+
+        /// <summary>
+        /// You are not allowed to change this function.
+        /// </summary>
+        public void TickCounter()
+        {
+            float timeNow = Time.timeSinceLevelLoad;
+            float delta = timeNow - lastTime;
+            if (delta >= waitDuration)
+            {
+                while (delta >= waitDuration)
+                {
+                    Count++;
+                    delta -= waitDuration;
+                }
+                lastTime = timeNow;
+            }
+        }
+    }
+}
