6a5f278e8dc4fb142d9f507c3649f703e063d562
Kasper Christensen <kgc@bgs.dk>
2020-02-28 19:24:49 +0100

    Added additional instructions in WelcomeWindow

--- Assets/NotPartOfJobTest/Editor/WelcomeWindow.cs
+++ Assets/NotPartOfJobTest/Editor/WelcomeWindow.cs
@@ -15,6 +15,8 @@ namespace com.brainplus.jobtest.editor
             "We have prepared some assignments for you, to help us get a sense of your knowledge of Unity.",
             "Do as many of the assignments as you can - simply edit the files directly.",
             "It's ok if you can't fully finish all assignments, just get as close to the solution as you can.",
+            "There's no time limit so you can take all the time you need to finish the assignments.",
+            "Look for further instructions in the scripts with the same names as the scenes (instructions are shown in the inspector and written in the code)",
             "We recommend that you use Git so that you can track your own changes compared to the original code of the assignments.",
             "When we review your code we will be using Git, so it would be perfect if your code can be shared via a public Git repository on GitHub, BitBucket or similar.",
             "Here are some shortcuts to the scenes with the assignments:",
