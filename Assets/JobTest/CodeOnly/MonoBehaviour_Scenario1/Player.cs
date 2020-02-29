f82a6958fd16ed0ced638d527dee65cab98e7d5b
Joe Osborne <3324453+ettmetal@users.noreply.github.com>
2020-02-29 11:51:17 +0100

    MonoBehaviour_1 solution

--- Assets/JobTest/CodeOnly/MonoBehaviour_Scenario1/Player.cs
+++ Assets/JobTest/CodeOnly/MonoBehaviour_Scenario1/Player.cs
@@ -10,10 +10,11 @@ namespace com.brainplus.jobtest.monobehaviour.scenario1
         /// </summary>
         public Inventory Inventory { get; private set; }
 
-        void Start()
+        void Awake() // Awake can safely be used for setting up references to components atached to the same GameObject
         {
             CacheComponentReferences();
         }
+        // This is preferred to setting script execution order, as it requires less maintenance
 
         /// <summary>
         /// You are not allowed to change this function.
